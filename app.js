355e193bfee208d21c5ef12b8fca879bfa08ad70
jion-kozono <zeon66190329@gmail.com>
2020-06-24 12:17:54 +0900

    deta null error

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const graphqlHTTP = require('express-graphql')
+const mongoose = require("mongoose");
+const schema = require('./schema/schema')
 const app = express()
 
+mongoose.connect('mongodb+srv://test:test1234@cluster0-ro7la.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('db concected')
+})
 app.use('/graphql', graphqlHTTP({
-
+    schema,
+    graphiql: true
 }))
 app.listen(4000, () => {
     console.log('listning 4000 port')
